bede0d843898a8ec3beb4feeef29756b1d265c25
saprobum <sapuiprosenjit007@gmail.com>
2025-03-20 22:30:46 +0530

    portfolio link

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = ()=> {
       >
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/about">Portfolio</Link>
         <Link to="/contact">Contact</Link>
       </Stack>
 
